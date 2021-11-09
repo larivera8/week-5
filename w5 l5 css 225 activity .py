404132dcff91154c946dc42a355ac5c1a2ea0500
larivera8 <92804930+larivera8@users.noreply.github.com>
2021-11-09 10:51:00 -0600

    Add files via upload

---
+++ w5 l5 css 225 activity .py
@@ -0,0 +1,57 @@
+#Luis Rivera
+#10/30/21
+
+#problem1
+for x in range (100):
+#range is here which define 1 thru 100
+    print("hello world")
+
+#problem 2
+numbers = [12,10,32,3,66,17,42,99,20]
+for i in numbers:
+    print (i)
+for i in numbers:
+    sqr = i * i
+    print ("square of", i, "is",sqr )
+# Algorithm for square
+
+#Problem 3
+
+import turble
+lu = turtle.screen()
+lui = turtle.Turtle()
+
+
+lines = input("how many lines")
+number = input("how many sides")
+long = input("how long are the lines")
+shape = int(input("color of the shape")
+
+
+#problem 4
+for i in range(0,50):
+#ranges from 1 to 50
+    if i % 3 == 0
+        print (i," its divisible by three")
+    elif i % 5 == 0:
+        print (i, "both are divisible by 5")
+    else:
+        print (i)
+
+    if i %3 == 0 and i % 5 == 0:
+        print (i,"divisible by both")   
+               
+#problem 5
+import turtle
+lu = turtle.screen()
+lui = turtle.turtle
+color = {"black,green,yellow,brown,grey,red"}
+
+lui.start_fill()
+for i in range(168)
+    lui.forward(8=i/2)
+    lui.right(21-i/6
+    lui.speed (29)
+    lui.color(color[i%8])
+lui.finish_fill
+lu.exitwithclick()
